05c8e5d3936171ab67435dd6c43f910e4fd0002a
LET_137 <bjs.6300.137@gmail.com>
2024-07-28 09:27:36 +0900

    Initial Commit

---
+++ TestConverted/TestConvertedApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestConvertedApp.swift
+//  TestConverted
+//
+//  Created by 津本拓也 on 2024/07/28.
+//
+
+import SwiftUI
+
+@main
+struct TestConvertedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
